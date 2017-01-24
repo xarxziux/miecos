a268b604534191b79c01cc68d3da4703e60c71a8
xarxziux <hairy.one@virgin.net>
2017-01-24 00:23:28 +0000

    Minor corrections to .gitignore

--- src/0_base/miecos.js
+++ src/0_base/miecos.js
@@ -8,10 +8,12 @@ console.log ('myBlade =', JSON.stringify (myBlade));
 console.log ('myBlade.getHealth() = ', myBlade.getHealth());
 console.log ('myBlade.getMaxHealth() = ', myBlade.getMaxHealth());
 console.log ('myBlade.health = ', myBlade.health);
-console.log ('myBlade.maturityLevel = ', myBlade.maturityLevel);
+console.log ('myBlade.getMaturityLevel = ', myBlade.getMaturityLevel());
 console.log ('prototype = ', JSON.stringify (Object.getPrototypeOf (myBlade)));
 console.log ('myBlade.isVisible() = ', myBlade.isVisible());
-console.log ('myBlade.category) = ', myBlade.category);
+console.log ('myBlade.category = ', myBlade.category);
 console.log ('myBlade.head = ', myBlade.head);
+
+const newBlade = myBlade.
 
 
